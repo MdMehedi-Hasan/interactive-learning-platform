0e4055f24a048558bdc1faa0a5febf0114a1c7b6
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-27 15:38:05 +0600

    project initialized

---
+++ frontend/src/Pages/ClassRoomPage/Class.jsx
@@ -0,0 +1,9 @@
+const Class = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Class;
---
+++ frontend/src/Pages/HomePage/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../../components/Common/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default Home;
