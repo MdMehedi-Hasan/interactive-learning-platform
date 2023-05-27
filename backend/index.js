88e9fc428ca573986f9fa4aa7ae33934e3fcd119
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-27 17:46:54 +0600

    func - list & video is dynamic now

--- backend/index.js
+++ backend/index.js
@@ -10,29 +10,33 @@ app.use(cors())
 const uri = `mongodb+srv://mehedi:${process.env.ACCESS_KEY}@cluster0.f06bdh4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
 });
-  
+
 async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        app.get('/', (req, res) => {
-            res.send('Hello World!')
-          })
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db("ilp");
+        const collection = database.collection("videos");
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        app.get('/', async(req, res) => {
+            const query = {}
+            const result = await collection.find(query).toArray();
+            res.send(result)
+        })
     } finally {
-      // Ensures that the client will close when you finish/error
-    //   await client.close();
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- frontend/src/Pages/ClassRoomPage/Class.jsx
+++ frontend/src/Pages/ClassRoomPage/Class.jsx
@@ -1,54 +1,44 @@
 import Navbar from "../../components/Common/Navbar";
 import '../../assets/css/class.css'
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Class = () => {
+    const [videos, setVideos] = useState([])
+
+    const playVideo = (id) => {
+        console.log(id)
+    }
+    useEffect(() => {
+        axios.get('http://localhost:3000/')
+            .then(function (response) {
+                // handle success
+                // console.log(response.data);
+                setVideos(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }, [])
     return (
         <section>
             <Navbar />
             <div className="flex justify-around">
                 <aside>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/6PEr3XTwBXU?modestbranding=1&rel=0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                        </aside>
-                        <main>
-                            <div className="card card-side bg-base-100 shadow-xl">
-                                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </div>
-                            <div className="card card-side bg-base-100 shadow-xl">
-                                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </div>
-                            <div className="card card-side bg-base-100 shadow-xl">
-                                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </div>
-                            <div className="card card-side bg-base-100 shadow-xl">
-                                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </div>
-                            <div className="card card-side bg-base-100 shadow-xl">
-                                <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </div>
-                        </main>
-                    </div>
-                </section>
-                );
+                    <iframe width="560" height="315" src={videos[0]?.link} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </aside>
+                <main>
+                    {videos.map(video=> <div key={video._id} className="card card-side bg-base-100 shadow-xl">
+                        <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title cursor-pointer" onClick={()=>playVideo(video._id)}>{video?.title}</h2>
+                        </div>
+                    </div>)}
+                </main>
+            </div>
+        </section>
+    );
 };
 
-                export default Class;
+export default Class;
