a3c1eda4bf970092a8c299b01e59858c5ca3c4d1
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-28 09:54:27 +0600

    style - nav & footer styled

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,27 +1,27 @@
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+        <div className="bg-blue-900">
+            <footer className="container mx-auto footer p-10 px-6 text-white">
                 <div>
-                    <img src="" alt="" />
-                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    <img src="https://interactivecares-courses.com/wp-content/uploads/2021/03/IC-logo-Telda.png" alt="" className="w-36"/>
+                    
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title text-white opacity-100">Services</span>
+                    <a className="link link-hover">UI/UX Design</a>
+                    <a className="link link-hover">Web Development</a>
+                    <a className="link link-hover">API Integration</a>
+                    <a className="link link-hover">SEO</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span className="footer-title text-white opacity-100">Courses</span>
+                    <a className="link link-hover">General</a>
+                    <a className="link link-hover">Exam Preparation</a>
+                    <a className="link link-hover">Freelancing</a>
+                    <a className="link link-hover">Foreign Study</a>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
+                    <span className="footer-title text-white opacity-100">Legal</span>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
